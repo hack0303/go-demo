11077aa8b953aa0e62ef7d45308698c321a285bb
hack0303 <1097037585@qq.com>
2021-04-14 22:10:31 +0800

    1.短路 多结果 动态变量

---
+++ src/main/constants.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func say1() {
+	fmt.Println("say 1")
+}
+
+func say2() {
+	fmt.Println("say 2")
+}
--- src/main/main.go
+++ src/main/main.go
@@ -7,6 +7,28 @@ import (
 	"time"
 )
 
+func say() {
+	fmt.Println("say")
+}
+
+func hasReturn() int {
+	return 100
+}
+
+func add(x int, y int) int {
+	return x + y
+}
+
+//https://tour.golang.org/basics/5 短路
+func add3(x, y, z int) int {
+	return x + y + z
+}
+
+//https://tour.golang.org/basics/6 多结果
+func swapMulti(p0, p1 string) (string, string) {
+	return p1, p0
+}
+
 //https://tour.golang.org/welcome/4
 func main() {
 	fmt.Println("hello world!")
@@ -14,5 +36,10 @@ func main() {
 	fmt.Println("rand is", rand.Intn(10))
 	fmt.Println("math.Squrt is", math.Sqrt(4))
 	fmt.Println("math.pi is not access,please use math.Pi", math.Pi)
-
+	//https://tour.golang.org/basics/4
+	fmt.Println("print some,", hasReturn(), add(1000, 1234))
+	say()
+	fmt.Println("短路", add3(1, 2, 3))
+	a, b := swapMulti("hello", "world")
+	fmt.Println("多结果", a, b)
 }
